003412e380b2ca93829f547809f6ca19e48aa50d
QUANDDO <dhquan1910@gmail.com>
2017-09-01 13:35:36 +0700

    shop fixed ui

--- src/app/authenticate/login/login.component.ts
+++ src/app/authenticate/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../shared/services/authenticate.service';
-import { User } from '../../shared/models/user' ;
+import { User } from '../../shared/models/User' ;
 
 @Component({
   selector: 'app-login',
