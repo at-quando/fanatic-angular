4022ca5a699664ab9926afc6e4ae95e24b29f5eb
PHU VIET <username@users.noreply.github.com>
2017-09-08 08:35:05 +0700

    angular feature history_v2

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AppService } from '../app.service'
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
+  providers: [AppService]
 })
 export class ShopComponent implements OnInit {
+  private sub: any;
+  private shopId: any;
 
-  constructor() { }
+
+  constructor(
+    private route: ActivatedRoute,
+    private _app: AppService
+  ) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.shopId = params['id'];
+      console.log(this.shopId);
+      this
+    });
   }
-
 }
